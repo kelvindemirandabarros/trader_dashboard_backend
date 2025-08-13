6b9ca6c0d9bea34fb7b2ed044adfc7a66d3d9644
Kelvin de Miranda Barros <kmbcg@hotmail.com>
2025-08-13 03:31:47 -0300

    fix: Correção de variável do JWT.
    - Correção do dotenv carregando variáveis em arquivo inexistente.

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,11 +1,11 @@
 const is_production_env = process.env.NODE_ENV === 'production';
 
 import dotenv from 'dotenv';
-dotenv.config({ path: is_production_env ? '.env' : '.env.test' });
+dotenv.config();
 
 export const auth_config = {
   jwt: {
-    secret: process.env.TOKEN_SECRET as string,
+    secret: process.env.JWT_SECRET as string,
     expires_in: '7d',
     utilities: {
       login: 'login'
